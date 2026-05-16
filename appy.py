6585e858f6b5edb9d4aa94ba9b799c8fae59cd41
NaLi <nadia.lizbeth9@gmail.com>
2026-05-15 19:07:35 -0600

    Fitro agregado de impacto por ciudad

--- appy.py
+++ appy.py
@@ -1,6 +1,9 @@
 #Inicie cargando streamlit desde la terminal de vs (no sabia que tenia una) y ahora la importo
 import streamlit as st
 from PIL import Image
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
 
 #Inicio mi prueba añadiendo un titulo 
 
@@ -31,6 +34,54 @@ col1, col2, col3 = st.columns([1, 2, 1])
 #y que en la 2da añada mi imagen, alo mejor uso lo de las columnas mas tarde
 with col2:
     st.image(N_image)
+#Aca hice un commit
+
+#Iniciamos con lo primero
+#cargamos el df
+#esto es pa cargarlo
+df = pd.read_csv(r"AI_Student_Life_Pakistan_2026.csv")
+
+#Aqui es pa ocultarlo si es q no lo queremos ver
+with st.expander('Dataset'):
+    df
+
+col11 , col12 = st.columns([1,2])
+
+with col11 :
+    st.header("Análisis de Impacto")
+    #Inicio a fragmentar el ds, esto es para el boton de seleccion solicitado, tomamos las columnas Impact_on_Grades y AI_Tool_Used
+    #Aca estan las opciones, le pedimos que añada una general para poder ver todas las ciudades , que tambien necesitaremos su columna
+    ciudades = ["Todas las ciudades"] + list(df["City"].unique())
+                                        #Aca le pido que haga una lista unicamente de la columna City tomando solo los valores unicos
+    #Ahora el seleccionador, este lo declaramos con selectbox y le ponemos como la instruccion de que debe
+    #Seleccionar el usuario y le damos las variables
+    ciudad_sel = st.selectbox("Selecciona una ciudad:", ciudades)
+    #Aca es para que en caso de seleccionar todas tome todas las ciudades muestre la grafica general
+    if ciudad_sel == "Todas las ciudades":
+        df_1 = df
+    else: #Sino hara un filtrado por ciudad, en base a la opcion elegida
+        df_1 = df[df["City"] == ciudad_sel]
+
+    # Filtramos el df pidiendo que nos agrupe en grupos las columnas de tool used y impact on grades y que nos de sus tamaños
+    anli_IAC = df_1.groupby(["AI_Tool_Used", "Impact_on_Grades"]).size()
+
+    # Las convertimos en columnas para la grafica
+    anli_IAC = anli_IAC.unstack()
+
+    # Graficamos
+    #Esto es para pedirle que ponga las graficas apiladas y no por separado
+    anli_IAC.plot(kind="bar", stacked=True)
+    plt.xlabel("Herramienta")
+    plt.ylabel("Cantidad de estudiantes") 
+
+    st.pyplot(plt) #Para que muestre la grafica, si no solo existe, pero no aparece
+
+#Listo ahora va 2do commit (o primer commit de modificaciones al code)
+    
+
+
+
+    
 
 
 
